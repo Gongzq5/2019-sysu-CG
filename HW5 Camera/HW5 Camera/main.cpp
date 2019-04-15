0b8df3cfd1d2fff7790fb3106f094f8ef4c7e1e4
GZQ <765194873@qq.com>
2019-04-15 18:17:24 +0800

    HW5

--- HW5 Camera/HW5 Camera/main.cpp
+++ HW5 Camera/HW5 Camera/main.cpp
@@ -4,33 +4,53 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-#include "Bresenham.h"
+#include "HW4.h"
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "Camera.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 800;
 
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor;\n"
-"out vec3 ourColor;\n"
+"layout (location = 1) in vec3 viColor;\n"
+"\n"
+"out vec3 vColor;\n"
+"\n"
+"uniform mat4 model;\n"
+"uniform mat4 view;\n"
+"uniform mat4 projection;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
-"   ourColor = aColor;\n"
-"}\0";
+"       gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
+"       vColor = viColor;\n"
+"}\n";
 
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 vColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = ourColor;\n"
-"}\n\0";
+"       FragColor = vec4(vColor, 0.8);\n"
+"}\n";
 
 
 int main()
@@ -41,6 +61,8 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	
 
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
@@ -69,6 +91,7 @@ int main()
 
 	ImVec4 clear_color = ImVec4(1.0f, 1.0f, 1.0f, 1.00f);
 
+	
 	// build and compile our shader program
 	// ------------------------------------
 	// vertex shader
@@ -109,66 +132,64 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// set up vertex data (and buffer(s)) and configure vertex attributes
-	// ------------------------------------------------------------------	
-	float vertices[] = {
-		1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-		-1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f
+	std::vector<float> cubeVertices{
+		// coordinate			// color
+		 2.0f,  2.0f,  2.0f,    1.0f, 0.0f, 0.0f,
+		 2.0f,  2.0f, -2.0f,    1.0f, 0.8f, 0.0f,
+		 2.0f, -2.0f,  2.0f,	0.0f, 1.0f, 0.6f,
+		 2.0f, -2.0f, -2.0f,	0.0f, 1.0f, 0.0f,
+		-2.0f,  2.0f,  2.0f,	1.0f, 0.4f, 0.0f,
+		-2.0f,  2.0f, -2.0f,	0.0f, 1.0f, 0.5f,
+		-2.0f, -2.0f,  2.0f,	0.5f, 0.0f, 1.0f,
+		-2.0f, -2.0f, -2.0f,	0.5f, 1.0f, 0.0f
 	};
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-	glBindVertexArray(VAO);
+	
+	std::vector<unsigned int> cubeIndices{
+		0, 1, 2,    1, 2, 3,
+		1, 3, 5,    3, 5, 7,
+		0, 1, 5,    0, 4, 5,
+		4, 5, 7,    4, 6, 7,
+		0, 2, 4,    2, 4, 6,
+		2, 3, 7,    2, 6, 7
+	};
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	unsigned int cubeVAO, cubeVBO, cubeEBO;
+	HW4::bind(cubeVertices, cubeVAO, cubeVBO);
+	HW4::bindEBO(cubeIndices, cubeVAO, cubeEBO);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	// —’…´ Ù–‘
+
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-
-	// Bresenham algorithm
-	int triVertices[6] = {-90, 20, 90, 70, 10, -50};
-	
-	//auto triPoints = Bresenham::genTriPoints(Bresenham::Point(triVertices[0], triVertices[1]),
-	//	Bresenham::Point(triVertices[2], triVertices[3]),
-	//	Bresenham::Point(triVertices[4], triVertices[5]));
-	auto triPoints = Bresenham::genTriPoints(Bresenham::Point(90, 90),
-			Bresenham::Point(-50, 20),
-			Bresenham::Point(-8, 100));
-
-	unsigned int HW3B1_VAO, HW3B1_VBO;
-	Bresenham::pointsBindVAO(HW3B1_VAO, HW3B1_VBO, triPoints);
-
-	int radius = 50;
-	auto circlePoints = Bresenham::genCirclePositions(Bresenham::Point(0, 0), radius);
-	unsigned int HW3B2_VAO, HW3B2_VBO;
-	Bresenham::pointsBindVAO(HW3B2_VAO, HW3B2_VBO, circlePoints);
-
-	auto filledTriPoints = Bresenham::genFilledTriPoints(Bresenham::Point(triVertices[0], triVertices[1]),
-		Bresenham::Point(triVertices[2], triVertices[3]),
-		Bresenham::Point(triVertices[4], triVertices[5]));
-	unsigned int HW3N1_VAO, HW3N1_VBO;
-	Bresenham::pointsBindVAO(HW3N1_VAO, HW3N1_VBO, filledTriPoints);
 
 	// as we only have a single shader, we could also just activate our shader once beforehand if we want to 
 	glUseProgram(shaderProgram);
 
 	enum Homework_number {
-		HW3_Basic1,
-		HW3_Basic2,
-		HW3_Bonus1
+		B1, B2, B3, B4, O1
 	};
-	int HW_choose = HW3_Basic1;
+	int HW_choose = B1;
+	float direct[2] = { 0.0f, 0.0f };
+	float ortho_cordinate[3][3] = { { -10.0f, 10.0f, -10.0f },{ 10.0f, -50.0f, 50.0f },{ -30.0f, 0.0f, 0.0f } };
+
+	// retrieve the matrix uniform locations
+	unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+	unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+	unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+
+	glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 projection = glm::ortho(1.0f, 1.0f, 1.0f, 1.0f, 0.1f, 100.0f);
 	
+
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
 	{
+		deltaTime = glfwGetTime() - lastFrame;
+		lastFrame = glfwGetTime();
+
 		// input
 		// -----
 		processInput(window);
@@ -177,22 +198,15 @@ int main()
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 		{
-			ImGui::Begin("Choose radio to toggle different homework.");      
-			ImGui::RadioButton("HW3 Basic1", &HW_choose, HW3_Basic1);
-			ImGui::RadioButton("HW3 Basic2", &HW_choose, HW3_Basic2);
-			if (HW_choose == HW3_Basic2) {
-				ImGui::Text("Radius");
-				ImGui::SliderInt("", &radius, 0, 100);
-			} else if (HW_choose == HW3_Bonus1 || HW_choose == HW3_Basic1) {
-				ImGui::Text("Choose three vertices.");
-				ImGui::SliderInt("x1", &triVertices[0], -100, 100);
-				ImGui::SliderInt("y1", &triVertices[1], -100, 100);
-				ImGui::SliderInt("x2", &triVertices[2], -100, 100);
-				ImGui::SliderInt("y2", &triVertices[3], -100, 100);
-				ImGui::SliderInt("x3", &triVertices[4], -100, 100);
-				ImGui::SliderInt("y3", &triVertices[5], -100, 100);
+			ImGui::Begin("Choose radio to toggle different homework.");
+			ImGui::RadioButton("Basic 1 Projection", &HW_choose, B1);
+			if (HW_choose == B1) {
+				ImGui::SliderFloat3("orthographic parameters 1", ortho_cordinate[0], -50.0f, 50.0f);
+				ImGui::SliderFloat3("orthographic parameters 2", ortho_cordinate[1], -50.0f, 50.0f);
+				ImGui::SliderFloat("Perspect pos", &ortho_cordinate[2][0], -50.0f, 10.0f);
 			}
-			ImGui::RadioButton("HW3 Bonus1", &HW_choose, HW3_Bonus1);
+			ImGui::RadioButton("Basic 2 View Changing", &HW_choose, B2);
+			ImGui::RadioButton("Bonus  ", &HW_choose, O1);
 			ImGui::End();
 		}
 		ImGui::Render();
@@ -206,50 +220,83 @@ int main()
 		// ------
 
 		// update shader uniform
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		glUniform4f(vertexColorLocation, clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		//glUniform4f(vertexColorLocation, clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		glEnable(GL_DEPTH_TEST);
 
-		if (HW_choose == HW3_Basic1) {
-			triPoints = Bresenham::genTriPoints(Bresenham::Point(triVertices[0], triVertices[1]),
-				Bresenham::Point(triVertices[2], triVertices[3]),
-				Bresenham::Point(triVertices[4], triVertices[5]));
+		if (HW_choose == B1) {
+			model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+			view = glm::mat4(1.0f);
 
-			glBindVertexArray(HW3B1_VAO);
-			glBindBuffer(GL_ARRAY_BUFFER, HW3B1_VBO);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * triPoints.size(), triPoints.data(), GL_DYNAMIC_DRAW);
-			glPointSize(3);
+			projection = glm::ortho(ortho_cordinate[0][0], ortho_cordinate[0][1], ortho_cordinate[0][2], 
+				ortho_cordinate[1][0], ortho_cordinate[1][1], ortho_cordinate[1][2]);
+			
+			model = glm::rotate(model, glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+			model = glm::translate(model, glm::vec3(-1.5f, 0.5f, -1.5f));
+			view = glm::translate(view, glm::vec3(0.0f, 0.0f, -30.0f));
+			// projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+			
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+			glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+			
+			// printf("choose B1\n");
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			glEnable(GL_DEPTH_TEST);
-			glDrawArrays(GL_POINTS, 0, triPoints.size() / 3);
-		} else if (HW_choose == HW3_Basic2) {
-
-			circlePoints = Bresenham::genCirclePositions(Bresenham::Point(0, 0), radius);
 			
-			glBindVertexArray(HW3B2_VAO);
-			glBindBuffer(GL_ARRAY_BUFFER, HW3B2_VBO);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * circlePoints.size(), circlePoints.data(), GL_DYNAMIC_DRAW);
+			glBindVertexArray(cubeVAO);
 			
-			glPointSize(3);
-
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			glEnable(GL_DEPTH_TEST);
-			glDrawArrays(GL_POINTS, 0, circlePoints.size() / 3);
-		} else if (HW_choose == HW3_Bonus1) {
-			
-			filledTriPoints = Bresenham::genFilledTriPoints(Bresenham::Point(triVertices[0], triVertices[1]),
-				Bresenham::Point(triVertices[2], triVertices[3]),
-				Bresenham::Point(triVertices[4], triVertices[5]));
-			
-			glBindVertexArray(HW3N1_VAO);
-			glBindBuffer(GL_ARRAY_BUFFER, HW3N1_VBO);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * filledTriPoints.size(), filledTriPoints.data(), GL_DYNAMIC_DRAW);
-			
-			glPointSize(3);
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			glEnable(GL_DEPTH_TEST);
-			glDrawArrays(GL_POINTS, 0, filledTriPoints.size() / 3);
+			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 		}
-		
+		else if (HW_choose == B2) {
+			model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+			view = glm::mat4(1.0f);
+			projection = glm::mat4(1.0f);
+			
+			model = glm::rotate(model, glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+
+			float radius = 10.0f;
+			float camX = sin(glfwGetTime()) * radius;
+			float camZ = cos(glfwGetTime()) * radius;
+
+			view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+			
+			projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+			
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+			glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+			
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			
+			glBindVertexArray(cubeVAO);
+			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+		}
+		else {
+			glfwSetCursorPosCallback(window, mouse_callback);
+			glfwSetScrollCallback(window, scroll_callback);
+
+			model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+			view = glm::mat4(1.0f);
+			projection = glm::mat4(1.0f);
+
+			model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
+			model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
+			view = camera.GetViewMatrix();
+
+			projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+			
+			// transform ...
+			
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+			glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			glBindVertexArray(cubeVAO);
+			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+		}
+
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		glfwMakeContextCurrent(window);
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -258,17 +305,11 @@ int main()
 		glfwPollEvents();
 	}
 
-	//delete []HW3B1_vertices;
 	// optional: de-allocate all resources once they've outlived their purpose:
 	// ------------------------------------------------------------------------
-	//glDeleteVertexArrays(1, &HW2B3_VAO);
-	//glDeleteBuffers(1, &HW2B3_VBO); 
-	glDeleteVertexArrays(1, &HW3B1_VAO);
-	glDeleteBuffers(1, &HW3B1_VBO);
-	glDeleteVertexArrays(1, &HW3N1_VAO);
-	glDeleteBuffers(1, &HW3N1_VBO);
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+
+	glDeleteVertexArrays(1, &cubeVAO);
+	glDeleteBuffers(1, &cubeVBO);
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
 	glfwTerminate();
@@ -277,11 +318,20 @@ int main()
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
+void processInput(GLFWwindow *window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.processKeyboard('w' , deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.processKeyboard('s', deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.processKeyboard('a', deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.processKeyboard('d', deltaTime);
 }
+
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
@@ -290,4 +340,30 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	// make sure the viewport matches the new window dimensions; note that width and 
 	// height will be significantly larger than specified on retina displays.
 	glViewport(0, 0, width, height);
+}
+
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	if (firstMouse) {
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+	camera.ProcessMouseScroll(yoffset);
 }
